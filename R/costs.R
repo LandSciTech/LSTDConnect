169ac0bc3540da28724e94a040a1dbf7add15fdc
Valentin <valentin.lucet@gmail.com>
2021-07-08 16:11:48 -0400

    fix more checks fir global bindinds

--- R/costs.R
+++ R/costs.R
@@ -21,33 +21,34 @@
 setCosts <- function(composites, landclasses, outDir, outName, costOptions, 
                      costScns = NULL, extraProtectionValue = c(), 
                      exemptionClasses = c("urban", "cropland", "cutblock")) {
+  
   # costScns=c("humanFootprintVaried.waterOmit");exemptionClasses=c("urban","cropland","cutblock");extraProtectionValue=c()
   if (is.null(costScns)) {
     costScns <- setdiff(names(costOptions), c("Landclass", "speciesType"))
     print(costScns)
   }
   costOptions$speciesType[is.na(costOptions$speciesType)] <- ""
-
+  
   outPaths <- list()
-  lcids <- unique(subset(landclasses, select = c(Landclass, LCID)))
-  outCostValues <- subset(costOptions, select = c(Landclass, speciesType))
+  lcids <- unique(subset(landclasses, select = c("Landclass", "LCID")))
+  outCostValues <- subset(costOptions, select = c("Landclass", "speciesType"))
   for (cc in costScns) {
     # cc=costScns[1]
     if (!is.element(cc, names(costOptions))) {
       stop("Cost scenario ", cc, " not recognized. Options are: ", 
            paste(setdiff(names(costList$costOptions), c("Landclass", "speciesType")), collapse = ","))
     }
-
+    
     if (is.element(cc, names(extraProtectionValue))) {
       protectionLevels <- extraProtectionValue[[cc]]
     } else {
       protectionLevels <- c(0)
     }
-
+    
     aCosts <- subset(costOptions, select = c("Landclass", "speciesType", cc))
     names(aCosts)[names(aCosts) == cc] <- "cost"
-    aCosts <- subset(aCosts, !is.na(cost))
-
+    aCosts <- subset(aCosts, !is.na(aCosts$cost))
+    
     spTypes <- unique(aCosts$speciesType)
     for (ee in protectionLevels) {
       for (sp in spTypes) {
@@ -57,14 +58,14 @@ setCosts <- function(composites, landclasses, outDir, outName, costOptions,
         } else {
           outNameC <- paste0(c(cc), collapse = ".")
         }
-
+        
         if (!is.na(sp) && (sp != "")) {
           outNameC <- paste0(c(outNameC, sp), collapse = ".")
         }
         print(paste("Calculating", outNameC, "costs."))
-
-        cCosts <- subset(aCosts, speciesType == sp, select = c("Landclass", "cost"))
-
+        
+        cCosts <- subset(aCosts, aCosts$speciesType == sp, select = c("Landclass", "cost"))
+        
         # apply extra degradation to non-protected natural areas
         if (ee != 0) {
           ee <- strsplit(ee, split = "_")[[1]]
@@ -82,13 +83,13 @@ setCosts <- function(composites, landclasses, outDir, outName, costOptions,
           stop("Landcover classes in costOptions not recognized: ", paste(oddLCs, sep = ","))
         }
         cCosts <- merge(cCosts, lcids)
-        rcl <- subset(cCosts, select = c(LCID, cost))
-
+        rcl <- subset(cCosts, select = c("LCID", "cost"))
+        
         outCosts <- composites
         outCosts <- raster::subs(outCosts, rcl)
         outPaths[[outNameC]] <- paste0(outDir, "/", outName, "_", outNameC, ".RData")
         save(outCosts, file = outPaths[[outNameC]])
-
+        
         # add costs to output table
         names(cCosts)[names(cCosts) == "cost"] <- outNameC
         cCosts$speciesType <- sp
@@ -96,7 +97,7 @@ setCosts <- function(composites, landclasses, outDir, outName, costOptions,
       }
     }
   }
-
+  
   write.csv(outCostValues, paste0(outDir, "/", outName, ".csv"))
   return(outPaths)
 }
@@ -107,12 +108,12 @@ getCostOptions <- function(costFile) {
   costOptions <- read.csv(costFile, stringsAsFactors = F)
   costOptionDescriptions <- costOptions[1, ]
   costOptions <- costOptions[2:nrow(costOptions), ]
-
+  
   optionCols <- setdiff(names(costOptions), c("Landclass", "speciesType"))
-
+  
   for (o in optionCols) {
     costOptions[[o]] <- as.numeric(costOptions[[o]])
   }
-
+  
   return(list(costOptions = costOptions, costOptionDescriptions = costOptionDescriptions))
 }
